de5015752afc19ec41f888806517f07eac42d48b
sw4mpd0nkey <sw4mpd0nkey@protonmail.com>
2026-09-05 19:11:13 -0400

    updating to get docker to run

--- services/PlatformService/Data/PrepDb.cs
+++ services/PlatformService/Data/PrepDb.cs
@@ -3,34 +3,36 @@ using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace PlatformService.Data
+namespace PlatformService.Data;
+
+public static class PrepDb
 {
-    public static class PrepDb
+    public static void PrepPopulation(IApplicationBuilder app)
     {
-        public static void PrepPopulation(IApplicationBuilder app)
+        using var serviceScope = app.ApplicationServices.CreateScope();
+        AppDbContext? context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+        if (context != null)
         {
-            using var serviceScope = app.ApplicationServices.CreateScope();
-            SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
-        }
-
-        private static void SeedData(AppDbContext context)
-        {
-            if (!context.Platforms.Any())
-            {
-                Console.WriteLine(("---> seeding data..."));
-
-                context.Platforms.AddRange(
-                    new Models.Platform() {Name = "DotNet", Publisher = "M$", Cost="Bajillions"}
-                );
-
-                context.SaveChanges();
-            }
-            else
-            {
-                Console.WriteLine("---> we already have data");
-            }
-
+            SeedData(context);
         }
     }
 
+    private static void SeedData(AppDbContext context)
+    {
+        if (!context.Platforms.Any())
+        {
+            Console.WriteLine(("---> seeding data..."));
+
+            context.Platforms.AddRange(
+                new Models.Platform() {Name = "DotNet", Publisher = "M$", Cost="Bajillions"}
+            );
+
+            context.SaveChanges();
+        }
+        else
+        {
+            Console.WriteLine("---> we already have data");
+        }
+
+    }
 }
--- services/PlatformService/obj/Debug/net10.0/PlatformService.AssemblyInfo.cs
+++ services/PlatformService/obj/Debug/net10.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cdbc90aba7bfa0d5cefef902c3397ab5a6c07e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f2e70c51f8e011e15fb19b61d5d699099e43c31")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
