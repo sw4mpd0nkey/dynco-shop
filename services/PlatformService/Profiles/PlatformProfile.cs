0b3c927324b39b63172426371560f74bbf45ed6f
sw4mpd0nkey <sw4mpd0nkey@protonmail.com>
2026-09-05 00:27:02 -0400

    added Profiles

---
+++ services/PlatformService/Profiles/PlatformProfile.cs
@@ -0,0 +1,14 @@
+using AutoMapper;
+using PlatformService.DTOs;
+using PlatformService.Models;
+
+namespace PlatformService.Profiles;
+
+public class PlatformProfile : Profile
+{
+    public PlatformProfile()
+    {
+        CreateMap<Platform, PlatformReadDto>();
+        CreateMap<PlatformCreateDto, Platform>();
+    }
+}
--- services/PlatformService/obj/Debug/net10.0/PlatformService.AssemblyInfo.cs
+++ services/PlatformService/obj/Debug/net10.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b306ed3d226f1d5b16a36cf70404ac906e7931ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d8b502ad9492d095f045fc3d7195330d84608fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
