6c2e27f7cfc1b26b108c416d5f83a93888eac563
sw4mpd0nkey <sw4mpd0nkey@protonmail.com>
2026-09-05 00:19:49 -0400

    added DTOs

--- services/PlatformService/DTOs/PlatformCreateDto.cs
+++ services/PlatformService/DTOs/PlatformCreateDto.cs
@@ -1,21 +1,13 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace PlatformService.Models;
+namespace PlatformService.DTOs;
 
-public class Platform
+public class PlatformCreateDto
 {
-    [Key]
-    [Required]
-    public int Id { get; set; }
-
     [Required]
     public string Name { get; set; }
-
     [Required]
     public string Publisher { get; set; }
-
     [Required]
     public string Cost { get; set; }
-
-
 }
