7f2e70c51f8e011e15fb19b61d5d699099e43c31
sw4mpd0nkey <sw4mpd0nkey@protonmail.com>
2026-09-05 18:16:28 -0400

    added dockerfile for platform service

--- services/PlatformService/obj/Debug/net10.0/PlatformService.AssemblyInfo.cs
+++ services/PlatformService/obj/Debug/net10.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ca86f7484d63c3b20b16f31197805d596654a6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cdbc90aba7bfa0d5cefef902c3397ab5a6c07e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
